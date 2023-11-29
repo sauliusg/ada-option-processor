3f3e693af3223ff0ebe1a1a0eb74aa69fb3a9e2b
saulius <saulius@87cb6670-23ac-4663-9759-be0d5da7d2a7>
2023-11-29 07:29:56 +0000

    trunk/ (saulius@kukutis) Preventing double-free when option value is reused. Quite clumsy...
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-option-processor/trunk@37 87cb6670-23ac-4663-9759-be0d5da7d2a7

--- src/free_options.adb
+++ src/free_options.adb
@@ -13,21 +13,54 @@ package body Free_Options is
    
    procedure Free_Option (Option : in out Option_Type) is
    begin
-      if Option.Value /= null then
-         if Option.Value.Option_Kind = STRING_OPT then
-            Free (Option.Value.String_Value);
-         end if;
-      end if;
-      Free (Option.Value);
       Free (Option.Long_Option);
       Free (Option.Help);
    end;
    
    -- -------------------------------------------------------------------------
    
+   type Deallocated_Block_Array is array (Positive range <>) of Option_Value_Access;
+   
+   procedure Add_Dealocated_Block
+     (
+      Block : Option_Value_Access;
+      DB : in out Deallocated_Block_Array;
+      Count : in out Natural
+     ) is
+   begin
+      Count := Count + 1;
+      DB (Count) := Block;
+   end;
+   
+   function Is_Deallocated
+     (
+      Block : Option_Value_Access;
+      DB : in out Deallocated_Block_Array;
+      Count : in out Natural
+     ) return Boolean is
+   begin
+      for I in reverse 1 .. Count loop
+         if Block = DB (I) then
+            return True;
+         end if;
+      end loop;
+      return False;
+   end;
+   
    procedure Free_Options (OA : in out Option_Array) is
+      Deallocated_Blocks : Deallocated_Block_Array (1 .. OA'Length);
+      Deallocated_Block_Count : Natural := 0;
    begin
       for Option of OA loop
+         if not Is_Deallocated (Option.Value, Deallocated_Blocks, Deallocated_Block_Count) then
+            Add_Dealocated_Block (Option.Value, Deallocated_Blocks, Deallocated_Block_Count);
+            if Option.Value /= null then
+               if Option.Value.Option_Kind = STRING_OPT then
+                  Free (Option.Value.String_Value);
+               end if;
+            end if;
+            Free (Option.Value);
+         end if;
          Free_Option (Option);
       end loop;
    end;
